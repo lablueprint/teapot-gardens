55fef3622da42c319340bf7f13fd950e07096b7d
Ryan Yeo <ryandc.yeo@gmail.com>
2024-11-21 17:58:18 -0800

    init

---
+++ app/index.jsx
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Garden Gangggggggg</Text>
+      <StatusBar style="auto" />
+      <Link href="/page1" style={{ color: "blue" }}>
+        Go to Page1
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
