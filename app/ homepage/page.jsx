876dcf8ea1d9f5db4a4299cf8933f5b5e2f1e31e
Dauniel <danieljchang5@gmail.com>
2024-11-26 20:32:57 -0800

    starting changes for homepage

---
+++ app/ homepage/page.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <View>
+      <Text>Home Page</Text>
+      <text> </text>
+    </View>
+  );
+};
+
+export default Page1;
+
+const styles = StyleSheet.create();
