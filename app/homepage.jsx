00b6e7ac1c4066ee871528127866ec2d24f9dfc5
Sophia Lee <sophiaylee23@gmail.com>
2025-01-16 10:20:27 -0800

    tamagotchi level skeleton, img changes based on xp

--- app/homepage.jsx
+++ app/homepage.jsx
@@ -4,11 +4,27 @@ import Placeholder from './homepage_components/Placeholder';
 import CustomCarousel from './homepage_components/carousel';
 import { upcomingEvents, programPages } from './homepage_components/data';
 import sample_logo from '../assets/sample_logo.png';
+import pichu from '../assets/pichu.jpg';
+import pikachu from '../assets/pikachu.jpg';
+import raichu from '../assets/raichu.jpg';
 
 export default function Homepage() {
+  const user = {
+    xp: 0,
+  }
+
+  let level_img;
+  if (user.xp < 1000) {
+    level_img = pichu;
+  } else if (user.xp <= 2000) {
+    level_img = pikachu;
+  } else {
+    level_img = raichu;
+  }
+
   return (
     <View style={styles.main_container}>
-        <Placeholder imageSource={sample_logo} />
+        <Placeholder imageSource={level_img} />
       <View style={styles.carousel_container}>
         <CustomCarousel data={upcomingEvents} />
       </View>
