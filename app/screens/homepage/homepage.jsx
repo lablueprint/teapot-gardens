70351f8ac9d7b12179749035a465c8aa8b88c644
Patrick Zhou <patrickxc.zhou@gmail.com>
2025-04-16 22:35:26 -0700

    trying to update database thing rn

--- app/screens/homepage/homepage.jsx
+++ app/screens/homepage/homepage.jsx
@@ -8,6 +8,7 @@ import sample_logo from '@assets/sample.png';
 import pichu from '@assets/pichu.jpg';
 import pikachu from '@assets/pikachu.jpg';
 import raichu from '@assets/raichu.jpg';
+import User from 'backend/models/UserModel';
 
 export default function Homepage() {
   const [userData, setUserData] = useState(null);
@@ -20,18 +21,83 @@ export default function Homepage() {
   const tempUserId = '6789f49f8e0a009647312c7a';
   const tempEventId = '67932a72413f4d68be84e592';
 
+  const today = new Date();
+  const currentDate = today.toISOString().split('T')[0];
+  const currentYear = today.getFullYear();
+  const currentMonth = (today.getMonth() + 1).toString().padStart(2, '0');
+  const currentDay = today.getDate().toString().padStart(2, '0'); 
+  const currentTime = today.toTimeString().split(' ')[0].slice(0, 5).split(':')[0] + today.toTimeString().split(' ')[0].slice(0, 5).split(':')[1];
+  const months = {
+    January: '01',
+    February: '02',
+    March: '03',
+    April: '04',
+    May: '05',
+    June: '06',
+    July: '07',
+    August: '08',
+    September: '09',
+    October: '10',
+    November: '11',
+    December: '12',
+  }
+  // console.log(currentYear, currentMonth, currentDay, currentTime); 
+  let eventdate, eventtime, eventmonth, eventday, eventyear, eventAMPM;
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         // fetching user data here
-        const userResponse = await axios.get('https://4289-131-179-95-17.ngrok-free.app/api/users/6789f49f8e0a009647312c7a');
-        const testResponse = await axios.get('https://4289-131-179-95-17.ngrok-free.app/api/events/678f315b8d423da67c615e95');
+        const userResponse = await axios.get('https://9cad-2607-f010-2a7-103f-bd39-11f5-b4e8-a209.ngrok-free.app/api/users/6789f49f8e0a009647312c7a');
+        const testResponse = await axios.get('https://9cad-2607-f010-2a7-103f-bd39-11f5-b4e8-a209.ngrok-free.app/api/events/678f315b8d423da67c615e95');
         setTestEvent(testResponse);
   
         if (userResponse.status === 200) {
           setUserData(userResponse.data);
           setUserAttendingEvents(userResponse.data.attendingEvents);
-          console.log(userResponse.data.attendingEvents);
+          for (let event of userResponse.data.attendingEvents) {
+            const eventDate = await axios.get('https://9cad-2607-f010-2a7-103f-bd39-11f5-b4e8-a209.ngrok-free.app/api/events/' + event);
+            // event date formatted like "February 20th 2024"
+            // event time formatted like "3:00 PM"
+            eventAMPM = eventDate.data.time.split(' ')[1];
+            eventtime = eventDate.data.time.split(' ')[0].split(':')[0] + eventDate.data.time.split(' ')[0].split(':')[1];
+            if (eventAMPM === 'PM') {
+              eventtime = parseInt(eventtime) + 1200;
+            }
+            eventdate = eventDate.data.date;
+            eventmonth = months[eventDate.data.date.split(' ')[0]];
+            eventday = eventDate.data.date.split(' ')[1].replace(/\D/g, '');
+            eventyear = eventDate.data.date.split(' ')[2];
+            // console.log(eventDate.data.title, eventdate, eventtime, eventmonth, eventday, eventyear, eventAMPM);
+            if (currentYear > eventyear) {
+              // event is in the past fs
+              //I NEED HELP WITH THIS PART
+              console.log(userResponse.data.attendingEvents, event);
+              // userResponse.data.attendedEvents.push(event);
+              // userResponse.data.attendingEvents.remove(event);
+            }
+            else if (currentYear === eventyear) {
+              if (currentMonth > eventmonth) {
+                // event is in the past fs
+              }
+              else if (currentMonth === eventmonth) {
+                if (currentDay > eventday) {
+                  // event is in the past fs
+                }
+                else if (currentDay === eventDay) {
+                  if (currentTime > eventtime) {
+                    // event is in the past fs
+                  }
+                  else {
+                    //its a future event
+                  }
+                }
+              }
+            }
+          }
+
+
+
         } else {
           console.warn("No attending events found for the user");
           setUserAttendingEvents([]);
@@ -57,6 +123,8 @@ export default function Homepage() {
     }
 
   }
+
+
 
   return (
     <ScrollView style={styles.main_container}>
